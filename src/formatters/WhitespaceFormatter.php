910aa2c53abe77a3d5881d400904d1799f643f73
gossi <github@gos.si>
2014-10-05 22:20:25 +0200

    beautified WhitespaceFormatter

--- src/formatters/WhitespaceFormatter.php
+++ src/formatters/WhitespaceFormatter.php
@@ -16,99 +16,117 @@ class WhitespaceFormatter extends AbstractSpecializedFormatter {
 		T_CATCH => 'catch'
 	];
 	
+	private static $SYNTAX = [
+		')' => 'close',
+		'(' => 'open',
+		',' => 'comma', 
+		';' => 'semicolon',
+		':' => 'colon',
+		'=>' => 'arrow',
+		'->' => 'arrow', // function invocation
+		'::' => 'doublecolon', // function invocation
+		'?' => 'questionmark'
+	];
+	
 	protected function doVisit(Token $token) {
-		$parens = $this->context->getParensContext();
-		$parensToken = $this->context->getParensTokenContext();
-		
-		// keywords
+		$this->applyKeywords($token);
+		$this->applyAssignments($token);
+		$this->applyOperators($token);
+		$this->applyPrefixPostfix($token);
+		$this->applyUnary($token);
+		$this->applySyntax($token);		
+	}
+	
+	private function applyKeywords(Token $token) {
 		if (in_array($token->type, Tokenizer::$KEYWORDS)) {
 			$this->defaultFormatter->hideToken();
 			$this->writer->write($token->contents . ' ');
-			return;
 		}
-
-		// assignments
+	}
+	
+	private function applyAssignments(Token $token) {
 		if (in_array($token->contents, Tokenizer::$ASSIGNMENTS)) {
 			$this->whitespaceBeforeAfter($token, 'assignment', 'assignments');
-			return;
 		}
-		
-		// operators
+	}
+	
+	private function applyOperators(Token $token) {
 		if (in_array($token->contents, Tokenizer::$OPERATORS)) {
 			$this->whitespaceBeforeAfter($token, 'binary', 'operators');
-			return;
 		}
-
-		// prefix and postfix operators
+	}
+	
+	private function applyPrefixPostfix(Token $token) {
 		if ($token->type == T_INC || $token->type == T_DEC) {
 			// pre
 			if ($this->nextToken->type == T_VAR) {
 				$this->whitespaceBeforeAfter($token, 'prefix', 'operators');
 			}
-				
+		
 			// post
 			else if ($this->prevToken->type == T_VAR) {
 				$this->whitespaceBeforeAfter($token, 'postfix', 'operators');
 			}
 		}
-		
-		// unary operator @TODO
-		
+	}
+	
 
-		// syntax
-		$beforeAfter = [
-			')' => 'close',
-			'(' => 'open',
-			',' => 'comma', 
-			';' => 'semicolon',
-			':' => 'colon',
-			'=>' => 'arrow',
-			'->' => 'arrow', // function invocation
-			'::' => 'doublecolon', // function invocation
-			'?' => 'questionmark'
-		];
-		
-		foreach ($beforeAfter as $contents => $key) {
-			if ($token->contents == $contents) {
-
-				// continue when semicolon and it is not tangled to a block statement
-				if ($token->contents == ';' && $parens != ContextManager::LEXICAL_BLOCK) {
-					continue;
-				}
-
-				// check context
-				$context = 'default';
-
-				// first check the context of the current line
-				if (!empty($this->line)) {
-					$context = $this->line;
-				} 
-
-				// is it a parens group?
-				else if ($parens == ContextManager::LEXICAL_GROUP) {
-					$context = 'grouping';
-				}
-
-				// a function call?
-				else if ($parens == ContextManager::LEXICAL_CALL) {
-					$context = 'function_invocation';
-				}
-
-				// field access?
-				else if ($contents === '->' || $contents === '::') {
-					$context = 'field_access';
-				}
-
-				// or a given block statement?
-				else if ($parens == ContextManager::LEXICAL_BLOCK
-						&& isset(self::$BLOCK_CONTEXT_MAPPING[$parensToken->type])) {
-					$context = self::$BLOCK_CONTEXT_MAPPING[$parensToken->type];
-				}
-
-				$this->whitespaceBeforeAfter($token, $key, $context);
+	/**
+	 * @TODO
+	 * @param Token $token
+	 */
+	private function applyUnary(Token $token) {
+	
+	}
+	
+	private function applySyntax(Token $token) {
+		if (array_key_exists($token->contents, self::$SYNTAX)) {
+			$key = self::$SYNTAX[$token->contents];
+			$parens = $this->context->getParensContext();
+			
+			// return when semicolon is not inside a block context
+			if ($token->contents == ';' && $parens != ContextManager::LEXICAL_BLOCK) {
+				return;
 			}
+			
+			// anyway find context and apply it
+			$context = $this->findContext($token);
+			$this->whitespaceBeforeAfter($token, $key, $context);
+		}
+	}
+	
+	private function findContext(Token $token) {
+		$parens = $this->context->getParensContext();
+		$parensToken = $this->context->getParensTokenContext();
+		$context = 'default';
+		
+		// first check the context of the current line
+		if (!empty($this->line)) {
+			$context = $this->line;
 		}
 		
+		// is it a parens group?
+		else if ($parens == ContextManager::LEXICAL_GROUP) {
+			$context = 'grouping';
+		}
+		
+		// a function call?
+		else if ($parens == ContextManager::LEXICAL_CALL) {
+			$context = 'function_invocation';
+		}
+		
+		// field access?
+		else if ($token->contents === '->' || $token->contents === '::') {
+			$context = 'field_access';
+		}
+		
+		// or a given block statement?
+		else if ($parens == ContextManager::LEXICAL_BLOCK
+				&& isset(self::$BLOCK_CONTEXT_MAPPING[$parensToken->type])) {
+			$context = self::$BLOCK_CONTEXT_MAPPING[$parensToken->type];
+		}
+		
+		return $context;
 	}
 
 	private function whitespaceBeforeAfter(Token $token, $key, $context = 'default') {
